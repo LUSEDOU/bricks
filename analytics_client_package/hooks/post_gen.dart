e31519e512bebd441427c3dfed755daeeb38040f
LUSEDOU <dolorier_uribe@hotmail.com>
2024-01-13 17:49:09 -0500

    feat(analytics_client_package): add hooks

--- analytics_client_package/hooks/post_gen.dart
+++ analytics_client_package/hooks/post_gen.dart
@@ -1,5 +1,13 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+Future<void> run(HookContext context) async {
+  await _runDartPubGet(context);
+}
+
+Future<void> _runDartPubGet(HookContext context) async {
+  final formatProgress = context.logger.progress('Running "flutter pub get"');
+  await Process.run('flutter', ['pub', 'get']);
+  formatProgress.complete();
 }
--- analytics_client_package/hooks/pre_gen.dart
+++ analytics_client_package/hooks/pre_gen.dart
@@ -10,14 +10,17 @@ enum Client {
 
 Future<void> run(HookContext context) async {
   await _generateInterface(context);
+  await _generateClient(context);
 }
 
 Future<void> _generateInterface(HookContext context) async {
   final formatProgress =
       context.logger.progress('Making brick analytics_client_interface');
-  final brick = Brick.version(
-    name: 'analytics_client_interface',
-    version: '0.1.0',
+  final brick = Brick.git(
+    GitPath(
+      'https://github.com/LUSEDOU/bricks.git',
+      path: 'analytics_client_interface',
+    ),
   );
   final generator = await MasonGenerator.fromBrick(brick);
   final directory = Directory(path.join(
@@ -29,7 +32,59 @@ Future<void> _generateInterface(HookContext context) async {
     fileConflictResolution: FileConflictResolution.overwrite,
   );
   await generator.hooks.postGen();
-  formatProgress.complete();
+  formatProgress.complete('Made brick analytics_client_interface');
+  context.logger.logFilesGenerated(files.length);
+}
+
+Future<void> _generateClient(HookContext context) async {
+  final client = _clientFromContext(context);
+  String name = client.name;
+  final formatProgress =
+      context.logger.progress('Making brick ${client.brick}');
+  final brick = Brick.git(
+    GitPath(
+      'https://github.com/LUSEDOU/bricks.git',
+      path: client.brick,
+    ),
+  );
+
+  final generator = await MasonGenerator.fromBrick(brick);
+  final vars = <String, dynamic>{'path': '../analytics_client'};
+
+  if (client == Client.custom) {
+    context.logger.info(
+      '\nYou have chosen to make a custom analytics client. '
+      'Please fill out the following information:',
+    );
+    name = context.logger.prompt(
+      'What is the name of your analytics client?',
+    );
+    vars['name'] = name;
+    vars['path_to_client'] = '../analytics_client/';
+    vars['package'] = context.logger.prompt(
+      'What is the package name of your analytics client?',
+    );
+    vars['version'] = context.logger.prompt(
+      'What is the version of your analytics client?',
+    );
+    final useFlutter = context.logger.prompt(
+      'Does the package need the flutter sdk? (y/N)',
+      defaultValue: 'N',
+    );
+    vars['use_flutter'] = useFlutter == 'y';
+  }
+
+  final directory = Directory(path.join(Directory.current.path,
+      'analytics_client', '${name.snakeCase}_analytics_client'));
+  final target = DirectoryGeneratorTarget(directory);
+  final files = await generator.generate(
+    target,
+    vars: vars,
+    logger: context.logger,
+    fileConflictResolution: FileConflictResolution.overwrite,
+  );
+  await generator.hooks.postGen();
+  formatProgress.complete('Made brick $name');
   context.logger.logFilesGenerated(files.length);
 }
 
@@ -49,30 +104,22 @@ extension on Client {
   String get name {
     switch (this) {
       case Client.firebase:
+        return 'firebase';
+      case Client.custom:
+        return '';
+    }
+  }
+
+  String get brick {
+    switch (this) {
+      case Client.firebase:
         return 'firebase_analytics_client';
       case Client.custom:
         return 'analytics_client';
     }
   }
-
-  String get directory {
-    switch (this) {
-      case Client.firebase:
-        return this.name;
-      case Client.custom:
-        return 'analytics_client';
-    }
-  }
-
-  String get version {
-    switch (this) {
-      case Client.firebase:
-      case Client.custom:
-      default:
-        return '0.1.0';
-    }
-  }
 }
+
 extension on Logger {
   void logFilesGenerated(int fileCount) {
     if (fileCount == 1) {
