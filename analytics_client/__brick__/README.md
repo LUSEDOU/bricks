daf791e22ad6e4964572374627627df99efdd03f
LUSEDOU <dolorier_uribe@hotmail.com>
2024-01-13 11:45:52 -0500

    fix(analytics_client): path conflict

--- analytics_client/__brick__/README.md
+++ analytics_client/__brick__/README.md
@@ -1,5 +1,5 @@
-# Analytics Client
+# {{name.snakeCase()}}_client
 
 [![style: very good analysis](https://img.shields.io/badge/style-very_good_analysis-B22C89.svg)](https://pub.dev/packages/very_good_analysis)
 
-A Generic Analytics Client Interface.
+A {{name.titleCase()}} implementation of the analytics client interface.
--- analytics_client/hooks/pre_gen.dart
+++ analytics_client/hooks/pre_gen.dart
@@ -4,8 +4,6 @@ void run(HookContext context) {
   final path = context.vars['path_to_client'] as String;
 
   // Change the &#x2F; to / in the path
-  context.logger.info('Path to client: $path');
   final newPath = path.replaceAll('&#x2F;', '/');
-  context.logger.info('Path to client: $newPath');
   context.vars['path_to_client'] = newPath;
 }
